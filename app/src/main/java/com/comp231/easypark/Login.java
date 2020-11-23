249e6bc7c3595bd4048ebc1af027a5522f62e056
mrtrinh5293 <mrtrinh5293@gmail.com>
2020-11-22 20:00:38 -0500

    added forgot password

--- app/src/main/java/com/comp231/easypark/Login.java
+++ app/src/main/java/com/comp231/easypark/Login.java
@@ -1,8 +1,10 @@
-package com.comp231.easypark;
+package com.example.loginlogoutauthentication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -11,9 +13,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.comp231.easypark.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -23,6 +29,7 @@ public class Login extends AppCompatActivity {
     public static final String TAG = "TAG";
     private EditText loginPageEmail, loginPagePassword;
     private Button loginPage_registerBtn,loginPage_loginBtn;
+    private TextView forgotTextLink;
     ProgressBar progressBar;
 
     FirebaseAuth fAuth;
@@ -39,6 +46,7 @@ public class Login extends AppCompatActivity {
         loginPage_loginBtn=findViewById(R.id.loginPage_loginBtn);
 
         loginPage_registerBtn = findViewById(R.id.loginPage_registerBtn);
+        forgotTextLink = findViewById(R.id.forgotPassword);
         fAuth = FirebaseAuth.getInstance();
         attachJavaObjectToXML();
 
@@ -76,9 +84,8 @@ public class Login extends AppCompatActivity {
 
                                         Log.d("User Iddsdddd: ",uid);
 
-//                                updateUI(user);
-                                        Toast.makeText(Login.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+//                                updateUI(user)
+                                        startActivity(new Intent(getApplicationContext(),PhoneNumberPage.class));
                                     }else {
                                         Toast.makeText(Login.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                         progressBar.setVisibility(View.GONE);
@@ -94,6 +101,49 @@ public class Login extends AppCompatActivity {
 
             }
         });
+
+        forgotTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final EditText resetMail = new EditText(v.getContext());
+                final AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                passwordResetDialog.setTitle("Reset Password ?");
+                passwordResetDialog.setMessage("Enter Your Email To Received Reset Link.");
+                passwordResetDialog.setView(resetMail);
+
+                passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // extract the email and send reset link
+                        String mail = resetMail.getText().toString();
+                        fAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Toast.makeText(Login.this, "Reset Link Sent To Your Email.", Toast.LENGTH_SHORT).show();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(Login.this, "Error ! Reset Link is Not Sent" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+
+                    }
+                });
+
+                passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // close the dialog
+                    }
+                });
+
+                passwordResetDialog.create().show();
+
+            }
+        });
+
     }
 
     private void moveToRegisterPage(){
