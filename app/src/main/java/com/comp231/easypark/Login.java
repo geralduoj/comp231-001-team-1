8c0a7deb8eb0cfc6f4d93466882a8ad42480fa84
Jyoti Jairath <jyotijairath07@gmail.com>
2020-11-30 18:25:56 -0500

    test
    test

--- app/src/main/java/com/comp231/easypark/Login.java
+++ app/src/main/java/com/comp231/easypark/Login.java
@@ -19,7 +19,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
+//hiiii
 public class Login extends AppCompatActivity {
     public static final String TAG = "TAG";
     public static FirebaseUser user;
