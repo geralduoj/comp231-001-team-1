7efa4164b980ed3b6eafeb8e8431c59c067845b9
Bernardo Castro <bernardoc544@gmail.com>
2021-03-11 19:00:52 -0500

    Create PasswordLengthTest.java
    Simple password length validation

---
+++ app/src/test/java/com/comp231/easypark/PasswordLengthTest.java
@@ -0,0 +1,13 @@
+package com.comp231.easypark;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PasswordLengthTest {
+    String test = "password";
+    @Test
+    public void passwordLenght_Test(){
+        assertEquals(8, test.length());
+        assert test.length() == 8;
+    }
+}
